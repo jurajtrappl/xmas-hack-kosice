b2b02b42b676e1dc741eb016e011f80f18f91a15
Juraj Trappl <juro.trappl@gmail.com>
2022-12-03 12:29:33 +0100

    removed blank line

--- 2.py
+++ 2.py
@@ -8,7 +8,6 @@ for i in range(1, N + 1):
     """
     The elves come in randomly and each of them switches the lights on exactly the trees that are multiples of the elf's number.
     """
-
     for j in range(i, N + 1, i):
         trees[j] = switch(trees[j])
 
